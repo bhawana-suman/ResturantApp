62e0d13151d0860b0f140f38097d079394fe2a4f
bhawana-suman <bhawana.suman6@gmail.com>
2022-08-14 12:26:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/resturantapp/createproduct.java
@@ -0,0 +1,14 @@
+package com.example.resturantapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class createproduct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_createproduct);
+    }
+}
